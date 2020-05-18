d195a98207290597fa538a3650430219fa5c3c9c
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-18 12:53:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(renderBooks(resp.json) => console.log(resp.json));
+}
 
+}
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
